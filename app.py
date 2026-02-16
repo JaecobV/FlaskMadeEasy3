0b9d7ec285a3a577d5cd0c9e73b0ffce054fd443
JaecobViloria <23389@burnside.school.nz>
2026-02-17 09:19:34 +1300

    database linked to code

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
 
+DATABASE = 'database1.db'
+
+#initialize app
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 @app.route('/')
 def home():
-    #home page
-    return "Hello Again!"
+    db = get_db()
+    cursor = db.cursor()
+    sql = "SELECT * FROM Cars"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    return str(rows)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
